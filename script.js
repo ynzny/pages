ff39152d8072a92d3debad2830037729046ee819
ynzny <144221104+ynzny@users.noreply.github.com>
2023-09-22 14:29:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+window.addEventListener('scroll', e => {
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content'
+})
